c42a364ce21aba70c49f0bbd207c8785fe6b590e
MC-ASUS\Mayur Chauhan <mayurchauhan1995@gmail.com>
2021-12-25 12:17:43 +0530

    Released: First Release v0.1

---
+++ Assets/ScreenshotManager/Scripts/Editor/ScreenshotManager.cs
@@ -0,0 +1,39 @@
+﻿#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ScreenshotManager {
+    
+    private const string PATH = "./Screenshots/";
+    private const string NAME = "Screenshot";
+    private const string EXTENSION = "png";
+    private const string SUFFIX = "_";
+    private const string DATE_FORMAT = "yyyy-MM-dd_HH-mm-ss";
+
+    
+    [MenuItem("Screenshot/Normal Quality &p")]
+    public static void TakeScreenshot() {    
+        string path = GetFilePath();
+        ScreenCapture.CaptureScreenshot(path, 1);
+        Debug.Log($"ScreenshotManager: Screenshot saved to {path}");
+    }
+
+    [MenuItem("Screenshot/High Quality &#p")]
+    public static void TakeScreenshotHQ() {
+        string path = GetFilePath();
+        ScreenCapture.CaptureScreenshot(path, 2);
+        Debug.Log($"ScreenshotManager: Screenshot saved to {path}");
+    }
+
+    private static string GetFilePath() {
+        if(!Directory.Exists(PATH)) {
+            Directory.CreateDirectory(PATH);
+            Debug.Log($"Created Directroy named {PATH}");
+        }
+        return $"{PATH}{NAME}{SUFFIX}{System.DateTime.Now.ToString(DATE_FORMAT)}.{EXTENSION}";
+    }
+
+ 
+}
+#endif
---
+++ README.md
@@ -0,0 +1,12 @@
+## ScreenshotManager - Unity3D
+
+#### Game Engine Version : 
+    Unity3D =>  2019.4+
+
+###### Description: 
+    Custom Editor Script for taking screenshots of the game view and saving them to a folder in Unity3D
+
+#### CHANGE LOG
+ver_0.1
+--------------------
+- Initial Release	
